6b9e8d73ec3c66fbb7e12c340d5860c0f3012a90
hanis-khairudin <s22a0031@siswa.umk.edu.my>
2025-11-01 14:19:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,18 +5,17 @@ import plotly.express as px
 # Page config
 st.set_page_config(page_title="Full-Time Student Social Media Usage", page_icon="📊", layout="wide")
 
-# Data loading
-@st.cache_data
-def load_data():
-    url = "https://raw.githubusercontent.com/hanis-khairudin/assignment_scivis2025/refs/heads/main/FULLTIME%20STUDENT%20USING%20SOCIAL%20MEDIA.csv"
+# --- 2. PAGE CONFIGURATION ---
+st.set_page_config(page_title="🧠🔬 Scientific Visualization")
+
+st.header("🧠 Scientific Visualization 🔬", divider="gray")
+st.write("Tutorial Scientific Visualization JIE42403 📊💡")
+st.write("Dataset 'Student Survey' by Razib Mustafiz from Kaggle")
+
+# --- 3. LOAD DATA FROM GITHUB ---
+url = "https://raw.githubusercontent.com/hanis-khairudin/assignment_scivis2025/refs/heads/main/FULLTIME%20STUDENT%20USING%20SOCIAL%20MEDIA.csv"
     return pd.read_csv(url)
 
-df = load_data()
-
-# Sidebar nav
-page = st.sidebar.radio(
-    "Navigate",
-    ["Demographics & E-learning", "Social Media Platforms", "Frequency of Visits"]
-)
-
-st.title("📚 Full-Time Student Social Media Usage Dashboard")
+try:
+    students_df = pd.read_csv(url)
+    st.dataframe(arts_df.head())
