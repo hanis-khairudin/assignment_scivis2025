19bb7879a97eb003d82bb61318037a69b698d39d
hanis-khairudin <s22a0031@siswa.umk.edu.my>
2025-11-01 14:30:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,18 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-# Page config
-st.set_page_config(page_title="Full-Time Student Social Media Usage", page_icon="📊", layout="wide")
+st.set_page_config(page_title="Learning Tech Insights", page_icon="📚", layout="wide")
 
-# --- 2. PAGE CONFIGURATION ---
-st.set_page_config(page_title="🧠🔬 Scientific Visualization")
-
-st.header("🧠 Scientific Visualization 🔬", divider="gray")
-st.write("Tutorial Scientific Visualization JIE42403 📊💡")
-st.write("Dataset 'Student Survey' by Razib Mustafiz from Kaggle")
-
-# --- 3. LOAD DATA FROM GITHUB ---
+# Load data from GitHub
 url = "https://raw.githubusercontent.com/hanis-khairudin/assignment_scivis2025/refs/heads/main/FULLTIME%20STUDENT%20USING%20SOCIAL%20MEDIA.csv"
+fulltime_students_df = pd.read_csv(url)
 
-try:
-    students_df = pd.read_csv(url)
-    st.dataframe(arts_df.head())
+# Sidebar navigation
+st.sidebar.title("📂 Objectives")
+page = st.sidebar.radio("Select Objective", [
+    "Objective 1",
+    "Objective 2",
+    "Objective 3"
+])
+
+st.title("📊 Analysis of Students’ Intention Toward Social Media & Emerging Tech")
